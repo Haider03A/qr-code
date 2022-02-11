b84ccd847920e1c34a00e6acf91a6384f39f9deb
Hayder Pc <you@example.com>
2022-02-11 18:21:06 +0300

    sdd

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const ejs = require('ejs')
+const excelToJson = require('convert-excel-to-json');
+const app = express()
+const routerHome = require('./routes/home')
+const routerError = require('./routes/error')
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.static('routes'))
+
+/* const result = excelToJson({ 
+    sourceFile: './public/data/dataExcel.xlsx' ,
+    header: {
+        rows: 2
+    }
+});
+ */
+
+app.use('/', routerHome)
+app.use('/', routerError)
+
+app.listen(port, () => {
+    console.log(`Server run on port http://localhost:${port}`);
+})
