2f25a21c25104b7051b93970375d51d13fb99bea
Hayder Pc <you@example.com>
2022-03-13 21:50:26 +0300

    dd

--- public/js/main.js
+++ public/js/main.js
@@ -10,6 +10,22 @@ inputFileForm.onsubmit = (e) => {
     })
 
 } */
+const butBargerIcon = document.querySelector('.button-berger-icon');
+const ulNav = document.querySelector('ul.perint-items');
+const buttonExport = document.querySelector('.button-export');
+
+
+document.addEventListener('click', e => {
+    e.target !== butBargerIcon &&
+        e.target !== ulNav &&
+        (butBargerIcon.dataset.clicked = false)
+
+})
+
+function showAndHaddin(theThis) {
+    theThis.setAttribute('data-clicked', theThis.dataset.clicked == 'true' ? theThis.dataset.clicked = false : theThis.dataset.clicked = true);
+    console.log(theThis);
+}
 
 
 const selectInputTypeSaction = document.querySelectorAll('main section.perint-section > section.select-input-type form')[0];
@@ -22,15 +38,15 @@ const qrcodeStyleSaction = document.querySelectorAll('main section.perint-sectio
 
 const showItemSaction = document.querySelectorAll('main section.perint-section > section.show-item')[0];
 
-selectInputTypeSaction.addEventListener('submit', function(e) {
+selectInputTypeSaction.addEventListener('submit', function (e) {
     e.preventDefault();
     const formData = new FormData(this)
     // formData.append('id', 11)
     fetch('/a', {
         method: 'POST',
-        body: JSON.stringify({id: 123})
+        body: JSON.stringify({ id: 123 })
     }).then(res => res.json())
         .then(data => {
-        console.log(data);
-    })
+            console.log(data);
+        })
 })
