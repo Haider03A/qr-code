c42350c97ac61918271fe8145550ade735e15955
Hayder Pc <you@example.com>
2022-03-14 21:14:02 +0300

    f

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,24 @@
-/* const inputFileForm = document.querySelector('.input-file form')
-const inputFile = document.querySelector('.input-file form input[type=file]')
-inputFileForm.onsubmit = (e) => {
-    e.preventDefault()
-    let formData = new FormData()
-    formData.append('file', inputFile.files[0])
-    fetch('/', {
-        method: 'post',
-        body: formData
+// Start public code
+const buttons = document.querySelectorAll('button')
+buttons.forEach(but => {
+    but.addEventListener('click', function (e) {
+        e.preventDefault();
     })
-
-} */
-const butBargerIcon = document.querySelector('.button-berger-icon');
-const ulNav = document.querySelector('ul.perint-items');
-const buttonExport = document.querySelector('.button-export');
-
-
-document.addEventListener('click', e => {
-    e.target !== butBargerIcon &&
-        e.target !== ulNav &&
-        (butBargerIcon.dataset.clicked = false)
-
 })
 
 function showAndHaddin(theThis) {
     theThis.setAttribute('data-clicked', theThis.dataset.clicked == 'true' ? theThis.dataset.clicked = false : theThis.dataset.clicked = true);
-    console.log(theThis);
 }
 
+// End public code
 
-const selectInputTypeSaction = document.querySelectorAll('main section.perint-section > section.select-input-type form')[0];
+const butBargerIcon = document.querySelector('.button-berger-icon');
+const buttonExport = document.querySelector('.button-export');
 
-const inputFileSaction = document.querySelectorAll('main section.perint-section > section.input-file')[0];
+butBargerIcon.addEventListener('click', function () {
+    showAndHaddin(this)
+})
 
-const enterManualSaction = document.querySelectorAll('main section.perint-section > section.enter-manual')[0];
-
-const qrcodeStyleSaction = document.querySelectorAll('main section.perint-section > section.qr-code-style')[0];
-
-const showItemSaction = document.querySelectorAll('main section.perint-section > section.show-item')[0];
-
-selectInputTypeSaction.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const formData = new FormData(this)
-    // formData.append('id', 11)
-    fetch('/a', {
-        method: 'POST',
-        body: JSON.stringify({ id: 123 })
-    }).then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
+buttonExport.addEventListener('click', function () {
+    showAndHaddin(this)
 })
