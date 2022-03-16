bf63f07b2183c11e3849dab1b13e5d7747fb399e
Hayder Pc <you@example.com>
2022-03-16 21:24:10 +0300

    from px

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,28 @@
 // Start public code
 const buttons = document.querySelectorAll('button')
-buttons.forEach(but => {
-    but.addEventListener('click', function (e) {
-        e.preventDefault();
-    })
-})
+buttons.forEach(but => but.addEventListener('click', e => e.preventDefault()))
 
-function showAndHaddin(theThis) {
-    theThis.setAttribute('data-clicked', theThis.dataset.clicked == 'true' ? theThis.dataset.clicked = false : theThis.dataset.clicked = true);
-}
+const showAndHaddin = theEle => theEle.setAttribute('data-clicked', theEle.dataset.clicked == 'true' ? theEle.dataset.clicked = false : theEle.dataset.clicked = true);
 
+const onClickDocumentHaddin = (e, eleForSwitch, eleCunOne, eleCunTwo, eleCunThree) => eleForSwitch.dataset.clicked !== 'false' && e.target !== eleCunOne && e.target !== eleCunTwo && e.target !== eleCunThree ? eleForSwitch.dataset.clicked = false : '';
 // End public code
 
-const butBargerIcon = document.querySelector('.button-berger-icon');
-const buttonAdvanced = document.querySelector('.button-advanced');
-const buttonExport = document.querySelector('.button-export');
 
-butBargerIcon.addEventListener('click', function () {
-    showAndHaddin(this)
-})
-buttonAdvanced.addEventListener('click', function () {
-    showAndHaddin(this)
-})
-buttonExport.addEventListener('click', function () {
-    showAndHaddin(this)
-})
+
+// Start navbar and header
+const butBargerIcon = document.querySelector('.button-berger-icon');
+const navBar = document.querySelector('.header-of-page nav ul');
+document.addEventListener('click', e => onClickDocumentHaddin(e, butBargerIcon, butBargerIcon, navBar));
+butBargerIcon.addEventListener('click', _ => showAndHaddin(butBargerIcon));
+// End navbar and header
+
+const buttonAdvanced = document.querySelector('.button-advanced');
+const styleAndTiemButtons = document.querySelector('.enter-manual .buttons .style-and-tiem-buttons');
+document.addEventListener('click', e => onClickDocumentHaddin(e, buttonAdvanced, buttonAdvanced, styleAndTiemButtons));
+buttonAdvanced.addEventListener('click', _ => showAndHaddin(buttonAdvanced))
+
+const buttonExport = document.querySelector('.button-export');
+const exportButtons = document.querySelector('.enter-manual .buttons .exports-buttons');
+document.addEventListener('click', e => onClickDocumentHaddin(e, buttonExport, buttonExport, exportButtons));
+buttonExport.addEventListener('click', _ => showAndHaddin(buttonExport))
+
