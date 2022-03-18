9fd8b7dc4e86457c29d723112311b9871ef0ab1f
Hayder Pc <you@example.com>
2022-03-18 22:50:10 +0300

    from pc 18/03/2022

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,6 @@ const showAndHaddin = theEle => theEle.setAttribute('data-clicked', theEle.datas
 const onClickDocumentHaddin = (e, eleForSwitch, eleCunOne, eleCunTwo, eleCunThree) => eleForSwitch.dataset.clicked !== 'false' && e.target !== eleCunOne && e.target !== eleCunTwo && e.target !== eleCunThree ? eleForSwitch.dataset.clicked = false : '';
 // End public code
 
-
-
 // Start navbar and header
 const butBargerIcon = document.querySelector('.button-berger-icon');
 const navBar = document.querySelector('.header-of-page nav ul');
@@ -24,10 +22,38 @@ buttonAdvanced.addEventListener('click', _ => showAndHaddin(buttonAdvanced))
 const buttonExport = document.querySelector('.button-export');
 const exportButtons = document.querySelector('.enter-manual .buttons .exports-buttons');
 document.addEventListener('click', e => onClickDocumentHaddin(e, buttonExport, buttonExport, exportButtons));
-buttonExport.addEventListener('click', _ => showAndHaddin(buttonExport))
+buttonExport.addEventListener('click', _ => showAndHaddin(buttonExport));
 
 const qrcodeStyleButtonOpen = document.querySelector('.enter-manual .buttons .style-and-tiem-buttons .style-button');
 const qrcodeStyleButtonClose = document.querySelector('.qr-code-style form .back-button');
 const qrcodeStyleBox = document.querySelector('.qr-code-style');
-qrcodeStyleButtonOpen.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox))
-qrcodeStyleButtonClose.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox))
+qrcodeStyleButtonOpen.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
+qrcodeStyleButtonClose.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
+
+const inputQrcodeValue = document.querySelector('.enter-manual form label.qr-value input');
+const inputQrcodeTitle = document.querySelector('.enter-manual form label.qr-title input');
+const inputQrcodeChecboxLook = document.querySelector('.enter-manual form label.checkbox-look input');
+const cloneValueQrcodeToQrcodeTitle = _ => {
+    inputQrcodeChecboxLook.checked &&
+        (inputQrcodeTitle.value = inputQrcodeValue.value);
+}
+inputQrcodeChecboxLook.addEventListener('click', _ => {
+    if (inputQrcodeChecboxLook.checked) {
+        inputQrcodeTitle.disabled = true;
+        cloneValueQrcodeToQrcodeTitle();
+        inputQrcodeValue.focus();
+    } else {
+        inputQrcodeTitle.disabled = false;
+        inputQrcodeTitle.focus();
+    };
+});
+inputQrcodeValue.addEventListener('keyup', _ => cloneValueQrcodeToQrcodeTitle());
+inputQrcodeValue.addEventListener('blur', _ => cloneValueQrcodeToQrcodeTitle());
+
+// Start Qrcode style
+const inputsColors = document.querySelectorAll('.qr-code-style form .inputs-box label.color input');
+inputsColors.forEach(input => {
+    input.addEventListener('keyup', _ => input.value = input.value.toUpperCase())
+    input.addEventListener('blur', _ => input.value = input.value.toUpperCase())
+})
+// End Qrcode style
