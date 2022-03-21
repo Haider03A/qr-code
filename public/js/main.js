f0a48709810cc358ad0c6598114095842afac858
Hayder Pc <you@example.com>
2022-03-21 22:42:09 +0300

    jk

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,27 @@ buttons.forEach(but => but.addEventListener('click', e => e.preventDefault()))
 const showAndHaddin = theEle => theEle.setAttribute('data-clicked', theEle.dataset.clicked == 'true' ? theEle.dataset.clicked = false : theEle.dataset.clicked = true);
 
 const onClickDocumentHaddin = (e, eleForSwitch, eleCunOne, eleCunTwo, eleCunThree) => eleForSwitch.dataset.clicked !== 'false' && e.target !== eleCunOne && e.target !== eleCunTwo && e.target !== eleCunThree ? eleForSwitch.dataset.clicked = false : '';
+
+const inputValidationNumber = async (value, minValue = 0, maxValue = 1) => {
+    let letters;
+    if (value) {
+        for (i = 0; i < value.length; i++) {
+            for (num = 0; num < 11; num++) {
+                await value[i] == num &&
+                    (letters ? letters = letters + value[i] : letters = value[i]);
+            }
+        }
+        letters = Number(letters);
+        if (letters < minValue) {
+            letters = minValue
+        } else {
+            letters > maxValue ? letters = maxValue : letters = letters;
+        }
+    } else {
+        letters = minValue
+    };
+    return letters
+}
 // End public code
 
 // Start navbar and header
@@ -27,8 +48,20 @@ buttonExport.addEventListener('click', _ => showAndHaddin(buttonExport));
 const qrcodeStyleButtonOpen = document.querySelector('.enter-manual .buttons .style-and-tiem-buttons .style-button');
 const qrcodeStyleButtonClose = document.querySelector('.qr-code-style form .back-button');
 const qrcodeStyleBox = document.querySelector('.qr-code-style');
+// Start qrcode style inputs elements 
+const qrcodeStyleWidthInput = document.querySelector('.qr-code-style form .qr-style-width input');
+const qrcodeStyleHeightInput = document.querySelector('.qr-code-style form .qr-style-height input');
+const qrcodeStyleMarginInput = document.querySelector('.qr-code-style form .qr-style-margin input');
+const qrcodeStyleColorInput = document.querySelector('.qr-code-style form .qr-style-color input');
+const qrcodeStyleBackgroundColorInput = document.querySelector('.qr-code-style form .qr-style-background input');
+// End qrcode style inputs elements
 qrcodeStyleButtonOpen.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
-qrcodeStyleButtonClose.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
+qrcodeStyleButtonClose.addEventListener('click', async _ => {
+    inputValidationNumber(qrcodeStyleWidthInput.value, 20, 700)
+    .then(a => qrcodeStyleWidthInput.value = a)
+
+    // console.log(inputValidationNumber());
+});
 
 const inputQrcodeValue = document.querySelector('.enter-manual form label.qr-value input');
 const inputQrcodeTitle = document.querySelector('.enter-manual form label.qr-title input');
