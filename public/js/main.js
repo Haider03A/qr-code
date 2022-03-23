d089cece057b088286363b1c6cf900b8a3af4bea
Hayder Pc <you@example.com>
2022-03-23 11:37:01 +0300

    from pc

--- public/js/main.js
+++ public/js/main.js
@@ -6,25 +6,30 @@ const showAndHaddin = theEle => theEle.setAttribute('data-clicked', theEle.datas
 
 const onClickDocumentHaddin = (e, eleForSwitch, eleCunOne, eleCunTwo, eleCunThree) => eleForSwitch.dataset.clicked !== 'false' && e.target !== eleCunOne && e.target !== eleCunTwo && e.target !== eleCunThree ? eleForSwitch.dataset.clicked = false : '';
 
-const inputValidationNumber = async (value, minValue = 0, maxValue = 1) => {
-    let letters;
-    if (value) {
-        for (i = 0; i < value.length; i++) {
-            for (num = 0; num < 11; num++) {
-                await value[i] == num &&
-                    (letters ? letters = letters + value[i] : letters = value[i]);
+const inputValidationNumber = (value, minValue = 0, maxValue = 1) => {
+    return new Promise((resolve, reject) => {
+        let letters;
+        if (value) {
+            for (i = 0; i < value.length; i++) {
+                for (num = 0; num < 10; num++) {
+                    value[i] == num &&
+                        (letters ? letters = letters + value[i] : letters = value[i]);
+                }
             }
-        }
-        letters = Number(letters);
-        if (letters < minValue) {
-            letters = minValue
+            letters = Number(letters);
+            if (!letters) {
+                letters = minValue
+            } else if (letters < minValue) {
+                letters = minValue
+            } else {
+                letters > maxValue ? letters = maxValue : letters = letters;
+            }
         } else {
-            letters > maxValue ? letters = maxValue : letters = letters;
-        }
-    } else {
-        letters = minValue
-    };
-    return letters
+            letters = minValue
+        };
+        
+        resolve(letters)
+    })
 }
 // End public code
 
@@ -48,20 +53,6 @@ buttonExport.addEventListener('click', _ => showAndHaddin(buttonExport));
 const qrcodeStyleButtonOpen = document.querySelector('.enter-manual .buttons .style-and-tiem-buttons .style-button');
 const qrcodeStyleButtonClose = document.querySelector('.qr-code-style form .back-button');
 const qrcodeStyleBox = document.querySelector('.qr-code-style');
-// Start qrcode style inputs elements 
-const qrcodeStyleWidthInput = document.querySelector('.qr-code-style form .qr-style-width input');
-const qrcodeStyleHeightInput = document.querySelector('.qr-code-style form .qr-style-height input');
-const qrcodeStyleMarginInput = document.querySelector('.qr-code-style form .qr-style-margin input');
-const qrcodeStyleColorInput = document.querySelector('.qr-code-style form .qr-style-color input');
-const qrcodeStyleBackgroundColorInput = document.querySelector('.qr-code-style form .qr-style-background input');
-// End qrcode style inputs elements
-qrcodeStyleButtonOpen.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
-qrcodeStyleButtonClose.addEventListener('click', async _ => {
-    inputValidationNumber(qrcodeStyleWidthInput.value, 20, 700)
-    .then(a => qrcodeStyleWidthInput.value = a)
-
-    // console.log(inputValidationNumber());
-});
 
 const inputQrcodeValue = document.querySelector('.enter-manual form label.qr-value input');
 const inputQrcodeTitle = document.querySelector('.enter-manual form label.qr-title input');
@@ -88,5 +79,31 @@ const inputsColors = document.querySelectorAll('.qr-code-style form .inputs-box 
 inputsColors.forEach(input => {
     input.addEventListener('keyup', _ => input.value = input.value.toUpperCase())
     input.addEventListener('blur', _ => input.value = input.value.toUpperCase())
+})
+
+// Start qrcode style inputs elements 
+const qrcodeStyleWidthInput = document.querySelector('.qr-code-style form .qr-style-width input');
+const qrcodeStyleHeightInput = document.querySelector('.qr-code-style form .qr-style-height input');
+const qrcodeStyleMarginInput = document.querySelector('.qr-code-style form .qr-style-margin input');
+const qrcodeStyleColorInput = document.querySelector('.qr-code-style form .qr-style-color input');
+const qrcodeStyleBackgroundColorInput = document.querySelector('.qr-code-style form .qr-style-background input');
+// End qrcode style inputs elements
+
+qrcodeStyleButtonOpen.addEventListener('click', _ => showAndHaddin(qrcodeStyleBox));
+qrcodeStyleButtonClose.addEventListener('click', _ => {
+    inputValidationNumber(qrcodeStyleWidthInput.value, 20, 700).then(value => qrcodeStyleWidthInput.value = value);
+    inputValidationNumber(qrcodeStyleHeightInput.value, 20, 700).then(value => qrcodeStyleHeightInput.value = value);
+    inputValidationNumber(qrcodeStyleMarginInput.value, 0, 15).then(value => qrcodeStyleMarginInput.value = value);
+});
+qrcodeStyleWidthInput.addEventListener('blur', function () {
+    inputValidationNumber(qrcodeStyleWidthInput.value, 20, 700).then(value => qrcodeStyleWidthInput.value = value);
+});
+
+qrcodeStyleHeightInput.addEventListener('blur', function () {
+    inputValidationNumber(qrcodeStyleHeightInput.value, 20, 700).then(value => qrcodeStyleHeightInput.value = value);
+});
+
+qrcodeStyleMarginInput.addEventListener('blur', function () {
+    inputValidationNumber(qrcodeStyleMarginInput.value, 20, 700).then(value => qrcodeStyleMarginInput.value = value);
 })
 // End Qrcode style
